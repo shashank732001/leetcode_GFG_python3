7f1f8236113d9a8b93ddaa5c7367719679236cd6
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-10-10 17:43:01 +0530

    Time: 3690 ms (14.49%), Space: 13.9 MB (62.45%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,19 +4,31 @@ class Solution:
         n =len(nums)
         res =[]
         for i in range(n-3):
+            
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1,n-2):
                 
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                
+                temp_sum = target-(nums[i]+nums[j])
                 l = j+1
 
                 r = n-1
                 while l < r :
-                    fsum = nums[i]+nums[j]+nums[l]+nums[r]
-                    if fsum == target:
+                    
+                    if (nums[l]+nums[r]==temp_sum):
                         a=[nums[i],nums[j],nums[l],nums[r]]
-                        if a not in res:
-                            res.append(a)
+                        
+                        res.append(a)
+                        while l<r and nums[l]==nums[l+1]:
+                            l+=1
+                        while l<r and nums[r]==nums[r-1]:
+                            r-=1
                         l+=1
-                    elif fsum<target:
+                        r-=1
+                    elif nums[l]+nums[r]<temp_sum:
                         l+=1
                     else:
                         r-=1
