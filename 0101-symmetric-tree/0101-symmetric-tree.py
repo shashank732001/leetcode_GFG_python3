f807fedda554e8c72b7ca621715d98748a6ab6e0
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-10-17 12:36:26 +0530

    Time: 70 ms (28.23%), Space: 13.9 MB (94.03%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,16 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        def sym(nodeleft,noderight):
-            if nodeleft and noderight and nodeleft.val==noderight.val:
-                return sym(nodeleft.right,noderight.left) and sym(nodeleft.left,noderight.right)
+        """DFS Recursive solution"""
+#         def sym(nodeleft,noderight):
+#             if nodeleft and noderight and nodeleft.val==noderight.val:
+#                 return sym(nodeleft.right,noderight.left) and sym(nodeleft.left,noderight.right)
 
             
-            return not nodeleft and not noderight
+#             return not nodeleft and not noderight
                
             
-        return sym(root.left,root.right)    
+#         return sym(root.left,root.right)  
+
+
+        """BFS solution"""
+        
+
+        leftq = deque([root.left])
+        rightq = deque([root.right])
+
+        while leftq and rightq:
+            nodeleft = leftq.popleft()
+            noderight= rightq.popleft()
+
+            if nodeleft and noderight:
+                if nodeleft.val!=noderight.val:
+                    return False
+
+                leftq.append(nodeleft.left)
+                leftq.append(nodeleft.right)
+                rightq.append(noderight.right)
+                rightq.append(noderight.left)
+
+            elif nodeleft or noderight:
+                return False
+        return not leftq and not rightq    
                 
