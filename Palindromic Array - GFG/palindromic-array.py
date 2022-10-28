f60e99355e5bcfc465ca6d1ce23051212a442d1e
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-10-28 08:52:59 +0530

    Added solution - LeetHub

---
+++ Palindromic Array - GFG/palindromic-array.py
@@ -0,0 +1,26 @@
+# Your task is to complete this function
+# Function should return True/False or 1/0
+def PalinArray(arr ,n):
+    for ele in arr:
+        if str(ele)!=str(ele)[::-1]:
+            return False
+    return True    
+    # Code here
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        if PalinArray(arr, n):
+            print(1)
+        else:
+            print(0)
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
