f8a8fdab09faa366e3ff62877e85f1116790f46d
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-10-28 18:58:09 +0530

    Added solution - LeetHub

---
+++ Pairs with specific difference - GFG/pairs-with-specific-difference.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    def maxSumPairWithDifferenceLessThanK(self, arr, N, K): 
+        # Your code goes here 
+        arr.sort(reverse = True)
+        ans = 0
+        i = 0
+        
+        while i<N-1:
+            if arr[i]-arr[i+1]<K:
+                ans+=arr[i]+arr[i+1]
+                i+=2
+            else:
+                i+=1
+        return ans        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        N = int(input())
+        arr = [int(x) for x in input().strip().split()]
+        K = int(input())
+        ob=Solution()
+        print( ob.maxSumPairWithDifferenceLessThanK(arr, N, K) )
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+# } Driver Code Ends
