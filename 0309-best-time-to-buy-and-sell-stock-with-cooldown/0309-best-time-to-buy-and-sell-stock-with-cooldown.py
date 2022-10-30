21d4458c55911ab6f7f26bade465d23c231f7680
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-10-30 18:31:41 +0530

    Time: 110 ms (19.00%), Space: 17.9 MB (29.22%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}  #stores key = (i , buying) and value = max profit
+        
+        #buying we shift i by 1
+        #selling we shift i by 2 coz of 1 compulsory cooldown
+        
+        def dfs(i,buying):
+            if i>=len(prices):
+                return 0
+            
+            if (i,buying) in dp:
+                return dp[(i,buying)]
+            
+            if buying:
+                buy = dfs(i+1,not buying) - prices[i]
+                cooldown = dfs(i+1,buying)
+                dp[(i,buying)] = max(buy,cooldown)
+            
+            else:
+                sell = dfs(i+2,not buying)+prices[i]
+                cooldown = dfs(i+1,buying)
+                dp[(i,buying)] = max(sell,cooldown)
+            return dp[(i,buying)]
+        
+        return dfs(0,True)
+                
+                
