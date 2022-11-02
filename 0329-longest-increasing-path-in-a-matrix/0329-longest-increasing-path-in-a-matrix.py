ace3961a904c417603e5d08599e81282e9ee7207
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-02 10:47:18 +0530

    Time: 2133 ms (5.00%), Space: 14.7 MB (94.28%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -3,30 +3,30 @@ class Solution:
         
         
         rows, cols = len(matrix), len(matrix[0])
-        visited = set()
-        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        # visited = set()
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         dp = [[-1 for _ in range(cols)] for _ in range(rows)]
         
         if rows <= 1 and cols <= 1:
             return 1
-        
-        def invalid(i, j):
-            return i < 0 or j < 0 or i == rows or j == cols
-        
+    
         def dfs(i, j):
             if dp[i][j] != -1: return dp[i][j]
             dp[i][j] = 1
-            for dx, dy in dirs:
-                r, c = i + dx, j + dy
-                if not invalid(r, c) and matrix[r][c] > matrix[i][j]:
-                    dp[i][j] = max(dp[i][j], 1 + dfs(r, c))
+            
+            
+            for dr,dc in directions:
+                row = i+dr
+                col = j+dc
+                if row in range(rows) and col in range(cols) and matrix[row][col]>matrix[i][j]:
+                    dp[i][j] = max(dp[i][j], 1 + dfs(row, col))
             
             return dp[i][j]
         
-        c = 0
+        ans  = 0
         for i in range(rows):
             for j in range(cols):
-                c = max(c, dfs(i, j))
+                ans = max(ans, dfs(i, j))
         
-        return c
+        return ans
         
