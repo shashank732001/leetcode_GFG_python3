8fd37ffdfd9899df3227b53f128392b9d185476f
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-04 18:15:31 +0530

    Added solution - LeetHub

--- Valid Substring - GFG/valid-substring.py
+++ Valid Substring - GFG/valid-substring.py
@@ -3,7 +3,28 @@
 class Solution:
     def findMaxLen(ob, S):
         # code here 
-        char_stack = []
+        # char_stack = []
+        # index_stack = []
+        # index_stack.append(-1)
+        # Len = 0
+        
+        # for i in range(len(S)):
+            
+        #     if S[i]=="(":
+        #         char_stack.append(S[i])
+        #         index_stack.append(i)
+                
+        #     else:
+        #         if char_stack:
+        #             char_stack.pop()
+        #             index_stack.pop()
+        #             Len = max(Len,i-index_stack[-1])
+        #         else:
+        #             index_stack.append(i)
+        # return Len            
+        
+        
+        
         index_stack = []
         index_stack.append(-1)
         Len = 0
@@ -11,17 +32,17 @@ class Solution:
         for i in range(len(S)):
             
             if S[i]=="(":
-                char_stack.append(S[i])
                 index_stack.append(i)
                 
             else:
-                if char_stack:
-                    char_stack.pop()
-                    index_stack.pop()
-                    Len = max(Len,i-index_stack[-1])
-                else:
+
+                index_stack.pop()
+                if not index_stack:
                     index_stack.append(i)
-        return Len            
+                    
+                else:
+                    Len = max(Len,i-index_stack[-1])
+        return Len 
                     
 
 
