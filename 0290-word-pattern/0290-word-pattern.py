98a7b73dc9231ee46d12ac4962358610d30ae60a
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-04 10:16:58 +0530

    Time: 33 ms (91.16%), Space: 13.9 MB (74.95%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        pattern = [i for i in pattern]
+        s = s.split()
+        lookup1 = {}
+        lookup2 = {}
+        
+        if len(s)!=len(pattern):
+            return False
+        
+        for i in range(len(s)):
+            if s[i] in lookup1:
+                if lookup1[s[i]]!=pattern[i]:
+                    return False
+            else:
+                lookup1[s[i]] = pattern[i]
+                
+        for i in range(len(s)):
+            if pattern[i] in lookup2:
+                if lookup2[pattern[i]]!=s[i]:
+                    return False
+            else:
+                lookup2[pattern[i]] = s[i]        
+        return True        
