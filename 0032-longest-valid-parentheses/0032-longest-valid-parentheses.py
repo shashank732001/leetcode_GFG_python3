6a82d4054d132542e3c57a51ff0b59ef716e103a
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-04 18:49:26 +0530

    Time: 48 ms (91.91%), Space: 14 MB (99.08%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,22 +1,58 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack = []
+#         stack = []
         
-        stack.append(-1)
-        ans  = 0
+#         stack.append(-1)
+#         ans  = 0
         
-        for i in range(len(s)):
-            if s[i]=="(":
+#         for i in range(len(s)):
+#             if s[i]=="(":
                 
-                stack.append(i)
-            else:
-                stack.pop()
-                if not stack:
-                    stack.append(i)
+#                 stack.append(i)
+#             else:
+#                 stack.pop()
+#                 if not stack:
+#                     stack.append(i)
                     
-                else:
-                    ans = max(ans,i-stack[-1])
-        return ans            
+#                 else:
+#                     ans = max(ans,i-stack[-1])
+#         return ans 
+
+        n = len(s)
+        opened = 0
+        closed = 0
+        ans = 0
+        
+        for i in range(n):
+            if s[i]=="(":
+                opened+=1
+            else:
+                closed+=1
+            
+            if opened==closed:
+                ans = max(ans,opened+closed)
+            elif closed>opened:
+                closed = 0
+                opened = 0
+                
+        opened = 0
+        closed = 0
+        
+        for i in range(n-1,-1,-1):
+            if s[i]=="(":
+                opened+=1
+            else:
+                closed+=1
+            
+            if opened==closed:
+                ans = max(ans,opened+closed)
+            elif opened>closed:
+                opened = 0
+                closed = 0
+        return ans        
+                
+                
+    
                     
                     
         
