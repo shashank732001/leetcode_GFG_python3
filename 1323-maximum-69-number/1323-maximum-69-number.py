6005f6a123518453bee838bb340d9035399e8913
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-07 12:46:07 +0530

    Time: 58 ms (41.91%), Space: 13.8 MB (95.47%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         num = [i for i in str(num)]
-        
+
         for i in range(len(num)):
             if num[i]=="6":
                 num[i]="9"
