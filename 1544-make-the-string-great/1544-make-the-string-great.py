a0b81c8857cffbc185bda0c0ba271df9c3c582c9
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-08 09:17:21 +0530

    Time: 32 ms (98.12%), Space: 14 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,20 +1,31 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        def operation(strg):
-            strg = list(strg)
-            for i in range(len(strg)-1):
-                # print(len(strg),i)
-                if (strg[i]==strg[i+1].lower() or strg[i].lower()==strg[i+1]) and strg[i]!=strg[i+1]:
-                    strg.pop(i)
-                    strg.pop(i)
-                    break
-            # print(strg)        
-            return "".join(strg)
+        # def operation(strg):
+#             strg = list(strg)
+#             for i in range(len(strg)-1):
+                
+#                 if (strg[i]==strg[i+1].lower() or strg[i].lower()==strg[i+1]) and strg[i]!=strg[i+1]:
+#                     strg.pop(i)
+#                     strg.pop(i)
+#                     break
+#             # print(strg)        
+#             return "".join(strg)
         
-        new_strg = operation(s)
+#         new_strg = operation(s)
         
-        while new_strg!=s:
-            s = new_strg
-            new_strg = operation(s)
-            # print(new_strg)
-        return new_strg    
+#         while new_strg!=s:
+#             s = new_strg
+#             new_strg = operation(s)
+            
+#         return new_strg    
+
+
+        stack = []
+        
+        for char in s:
+            
+            if stack and (stack[-1]==char.lower() or stack[-1].lower()==char) and stack[-1]!=char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)        
