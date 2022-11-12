88b1e09779b70c431649277764f6d847fb0e178d
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-12 06:06:29 +0530

    Added solution - LeetHub

---
+++ Circular tour - GFG/circular-tour.py
@@ -0,0 +1,39 @@
+'''
+    lis[][0]:Petrol
+    lis[][1]:Distance
+'''
+
+class Solution:
+    
+    #Function to find starting point where the truck can start to get through
+    #the complete circle without exhausting its petrol in between.
+    def tour(self,lis, n):
+        #Code here
+        start = 0
+        deficit = 0
+        balance = 0
+        
+        for i in range(n):
+            balance += lis[i][0]-lis[i][1]
+            
+            if balance<0:
+                deficit+=balance
+                start = i+1
+                balance = 0
+        return start if deficit+balance>=0 else -1        
+                
+            
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__': 
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr=list(map(int, input().strip().split()))
+        lis=[]
+        for i in range(1, 2*n, 2):
+            lis.append([ arr[i-1], arr[i] ])
+        print(Solution().tour(lis, n))
+    # Contributed by: Harshit Sidhwa
+# } Driver Code Ends
