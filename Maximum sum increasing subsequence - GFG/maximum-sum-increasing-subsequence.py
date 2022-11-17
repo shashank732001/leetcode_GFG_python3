ad459f17a3cfd1ff36ce753d4e8018fee19e59df
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-17 20:30:14 +0530

    Added solution - LeetHub

---
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+class Solution:
+	def maxSumIS(self, Arr, n):
+		# code here
+        
+        dp = [0]*(n)
+        
+        for i in range(n):
+            dp[i]=Arr[i]
+        
+        for i in range(1,n):
+            for j in range(0,i):
+                
+                if Arr[j]<Arr[i]:
+                    
+                    dp[i] = max(dp[i],Arr[i]+dp[j])
+        return max(dp)            
+                    
+                
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		Arr = [int(x) for x in input().split()]
+		ob = Solution()
+		ans = ob.maxSumIS(Arr,n)
+		print(ans)
+
+# } Driver Code Ends
