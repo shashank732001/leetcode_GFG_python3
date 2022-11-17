aaef71072f7508ebec710a4f1fb55b7f1027e5c5
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-17 13:11:43 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.py
@@ -3,27 +3,27 @@
 class Solution:
     def knapSack(self, N, W, val, wt):
         # code here
-        n = N
-        dp = [["." for i in range(W+1)] for j in range(n+1)]  #we taking "." as initial value ,we can consider anything
         
-        # initializing the value at n=0 or w=0,
-        #we are considering 0 beacuse it was our return value in base condition
-        for i in range(n+1):
+        dp = [["."]*(W+1) for i in range(N+1)]
+        
+        for i in range(N+1):
             for j in range(W+1):
                 if i==0 or j==0:
                     dp[i][j]=0
-                    
-        # now we fill the matrix solving all subproblems
-        # this is nothing but our choice tree
         
-        for i in range(1,n+1):
+        
+        for i in range(1,N+1):
             for j in range(1,W+1):
+                
                 if wt[i-1]<=j:
-                    dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j])
+                    
+                    dp[i][j] = max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j])
+                
                 else:
                     dp[i][j]=dp[i-1][j]
-                    
-        return dp[n][W]
+        
+        return dp[-1][-1]            
+
 
 
 #{ 
