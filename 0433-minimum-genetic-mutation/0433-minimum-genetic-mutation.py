4434696df9e2e70ed55a03cd03012f2a3f97605d
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-22 07:59:10 +0530

    Time: 35 ms (88.63%), Space: 13.9 MB (86.90%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,32 +1,68 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         
-        def diff(string1,string2):
-            diff_count = 0
-            
-            for i in range(len(string1)):
-                if string1[i]!=string2[i]:
-                    diff_count+=1
-            return diff_count
+        if end not in bank:
+            return -1
         
+        if start not in bank:
+            bank.append(start)
         
-        q = deque([(start,0)])
-        visited = set()
+        mut = collections.defaultdict(list)
         
+        for gene in bank:
+            for j in range(len(gene)):
+                pattern = gene[:j]+"*"+gene[j+1:]
+                mut[pattern].append(gene)
+        
+        q = deque([start])
+        done = set()
+        done.add(start)
+        res = 0
         
         while q:
-            string,change = q.popleft()
-            if string==end:
-                return change
-            
-            visited.add(string)
-            
-            for mut in bank:
+            for i in range(len(q)):
+                gene = q.popleft()
+                if gene==end:
+                    return res
                 
-                if (mut not in visited) and diff(mut,string)==1:
+                for j in range(len(gene)):
+                    pattern = gene[:j]+"*"+gene[j+1:]
+                    for next_gene in mut[pattern]:
+                        if next_gene not in done:
+                            done.add(next_gene)
+                            q.append(next_gene)
+            res+=1
+        return -1    
+        
+        
+        
+        
+#         def diff(string1,string2):
+#             diff_count = 0
+            
+#             for i in range(len(string1)):
+#                 if string1[i]!=string2[i]:
+#                     diff_count+=1
+#             return diff_count
+        
+        
+#         q = deque([(start,0)])
+#         visited = set()
+        
+        
+#         while q:
+#             string,change = q.popleft()
+#             if string==end:
+#                 return change
+            
+#             visited.add(string)
+            
+#             for mut in bank:
+                
+#                 if (mut not in visited) and diff(mut,string)==1:
                     
-                    q.append((mut,change+1))
-        return -1            
+#                     q.append((mut,change+1))
+#         return -1            
                     
             
             
