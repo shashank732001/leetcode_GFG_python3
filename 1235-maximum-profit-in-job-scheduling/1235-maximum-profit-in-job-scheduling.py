8d6feb8c6878c8368e051a2e2ecdb8a7fc946b30
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-11-26 08:24:33 +0530

    Time: 588 ms (92.73%), Space: 46.8 MB (23.98%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,25 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        jobs = list(zip(startTime,endTime,profit))
+        jobs.sort(key=lambda i : i[0])  #sort based on start time
+        
+        
+        @lru_cache(None)
+        
+        def helper(i):
+            
+            if i==n:
+                return 0
+            
+            j = i+1
+            
+            while j<n and jobs[i][1]>jobs[j][0]:
+                j+=1
+            
+            one = jobs[i][2]+helper(j)
+            two = helper(i+1)
+            
+            return max(one,two)
+        
+        return helper(0)
