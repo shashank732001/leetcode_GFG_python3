6b7e67c1c96784b165e6c4ca2892d31fbb3e104c
shashank732001 <72207774+shashank732001@users.noreply.github.com>
2022-12-03 18:20:15 +0530

    Time: 1538 ms (70.14%), Space: 15.3 MB (70.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,13 +11,13 @@ class Solution:
         
         for i in range(n-1,-1,-1):
             
-            nextdp = set()
+            nextdp = dp.copy()
             
             for t in dp:
                 if t+nums[i]==target:
                     return True
                 nextdp.add(t+nums[i])
-                nextdp.add(t)
+                # nextdp.add(t)
             dp = nextdp
         return True if target in dp else False    
             
